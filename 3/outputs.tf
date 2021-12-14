dce866e37ef133dbc17911862fa66d562d21ff39
Jhon Edison Castro Sanchez <edisoncastrosanchez@gmail.com>
2021-12-13 22:23:05 -0500

    Update outputs.tf

--- 3/outputs.tf
+++ 3/outputs.tf
@@ -1,4 +1,4 @@
 output "url" {
     description = "Browser URL for container site"
-    value       = join(":", ["http://localhost", tostring(81)])
+    value       = join(":", ["http://localhost", tostring(var.external_port)])
 }
