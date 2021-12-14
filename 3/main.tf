83d2bb1a544eed1ecd824d10ec6ec43ae4005310
Jhon Edison Castro Sanchez <edisoncastrosanchez@gmail.com>
2021-12-13 22:18:37 -0500

    Create main.tf

---
+++ 3/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "server" {
+    name = "nginx"
+    keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.server.latest
+    name  = "terraform-nginx"
+    ports {
+        internal = 80
+        external = 81
+        protocol = "tcp"
+    }
+}
